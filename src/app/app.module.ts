30e8e631957555dc819e9ae1fbf84910d860f348
Milad Raeisi <miladsoft@yahoo.com>
2024-05-26 12:26:57 +0400

    Add Theme Preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { OverlayscrollbarsModule } from "overlayscrollbars-ngx";
 import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
+import { SettingsComponent } from './components/settings/settings.component';
  
 @NgModule({
-  declarations: [AppComponent, NostrComponent, HomeComponent, ProjectsComponent, ThemeSwitcherComponent],
+  declarations: [AppComponent, NostrComponent, HomeComponent, ProjectsComponent, ThemeSwitcherComponent, SettingsComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule,OverlayscrollbarsModule],
   bootstrap: [AppComponent],
   providers: [
---
+++ src/app/components/settings/settings.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private renderer: Renderer2) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.loadSettings();
+      this.setupListeners();
+    }
+  }
+
+  loadSettings() {
+    let theme = this.getLocalStorageItem('theme');
+    if (theme) {
+      this.setTheme(theme);
+    }
+
+    let showBanner = this.getLocalStorageItem('showBanner');
+    if (showBanner === null || showBanner === undefined || showBanner === 'true') {
+      this.enableBanner();
+    } else {
+      this.disableBanner();
+    }
+
+    let hue = this.getLocalStorageItem('hue');
+    if (hue) {
+      this.setHue(hue);
+    } else {
+      this.setHue('250');
+    }
+  }
+
+  setupListeners() {
+    const slider = document.getElementById('colorSlider') as HTMLInputElement;
+    const presetList = document.getElementById('preset-list');
+    const showBannerCheck = document.querySelector("input[name=banner-checkbox]") as HTMLInputElement;
+    const output = document.getElementById('hueValue');
+
+    if (slider) {
+      slider.oninput = () => {
+        const hue = slider.value;
+        this.setHue(hue);
+        if (output) output.innerText = hue;
+      };
+    }
+
+    if (presetList) {
+      presetList.onclick = (event) => {
+        const target = event.target as HTMLElement;
+        const hue = target.dataset['hue'];
+        if (hue) {
+          this.setHue(hue);
+        }
+      };
+    }
+
+    if (showBannerCheck) {
+      showBannerCheck.addEventListener('change', () => {
+        if (showBannerCheck.checked) {
+          this.enableBanner();
+        } else {
+          this.disableBanner();
+        }
+      });
+
+      const r = document.documentElement;
+      if (r.getAttribute('showBanner') === 'true') {
+        showBannerCheck.checked = true;
+      }
+    }
+  }
+
+  setTheme(theme: string) {
+    if (this.isBrowser) {
+      document.documentElement.setAttribute('theme', theme);
+      this.setLocalStorageItem('theme', theme);
+    }
+  }
+
+  enableBanner() {
+    if (this.isBrowser) {
+      document.documentElement.setAttribute('showBanner', 'true');
+      this.setLocalStorageItem('showBanner', 'true');
+    }
+  }
+
+  disableBanner() {
+    if (this.isBrowser) {
+      document.documentElement.setAttribute('showBanner', 'false');
+      this.setLocalStorageItem('showBanner', 'false');
+    }
+  }
+
+  setHue(hue: string) {
+    if (this.isBrowser) {
+      const root = document.documentElement;
+      root.style.setProperty('--primary-hue', hue);
+      this.setLocalStorageItem('hue', hue);
+    }
+  }
+
+  private getLocalStorageItem(key: string): string | null {
+    if (this.isBrowser && typeof localStorage !== 'undefined') {
+      return localStorage.getItem(key);
+    }
+    return null;
+  }
+
+  private setLocalStorageItem(key: string, value: string): void {
+    if (this.isBrowser && typeof localStorage !== 'undefined') {
+      localStorage.setItem(key, value);
+    }
+  }
+}
