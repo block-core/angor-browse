d01ff00327b3f0718a4c0a70842f90f69c0bb2dc
Milad Raeisi <miladsoft@yahoo.com>
2024-08-09 18:31:45 +0400

    Fix npm bug

--- src/app/services/relay.service.ts
+++ src/app/services/relay.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SimplePool } from 'nostr-tools';
-import WebSocket from 'blockcore-ws';
+import WebSocket from '@blockcore/ws';
 
 @Injectable({
   providedIn: 'root'
